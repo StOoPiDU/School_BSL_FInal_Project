1c09792986526791c30663dc4f5bab4051dd6d35
CPereira2School <cpereira2@academic.rrc.ca>
2022-12-01 19:30:00 -0600

    Completing finishing touches on login_process.

--- login_process.php
+++ login_process.php
@@ -1,4 +1,10 @@
 <?php
+/*
+ * Final Project - Best Soccer League
+ * Name: Cedric Pereira
+ * Date: December 01, 2022
+ * Description: Login process for Final Project
+ */
     
     require_once('connect.php');
     //require_once('session.php');
