e88c62fc0ae9ebbf1ac11df67854ca6a70165419
CPereira2School <cpereira2@academic.rrc.ca>
2022-12-02 14:01:06 -0600

    Relocated session data from here.

--- connect.php
+++ connect.php
@@ -40,21 +40,5 @@
 
      session_start();
 
-      if(isset($_SESSION['user_id']))
-      {
-         $sessionUser = $_SESSION['user_id'];
 
-         $query = "SELECT * FROM user WHERE user_id = '$sessionUser'";
-         $statement = $db->prepare($query);
-         $statement->execute();
-         $row = $statement->fetch();
-
-         $_SESSION['admin'] = $row['admin'];
-         $_SESSION['user_id'] = $row['user_id'];
-         $_SESSION['username'] = $row['username'];
-         if($row['admin'] = 1)
-         {
-            $_Session['admin'] = true;
-         }
-      }
  ?>
