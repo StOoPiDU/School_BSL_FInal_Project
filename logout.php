5885d25b7fa7a8aa61d3f15a352faac40c30bd2f
CPereira2School <cpereira2@academic.rrc.ca>
2022-12-02 02:08:37 -0600

    Not destroying session(s)    >:(

--- logout.php
+++ logout.php
@@ -1,11 +1,13 @@
 <?php
 /*
- * Assignment 3
+ * Final Project - Best Soccer League
  * Name: Cedric Pereira
  * Date: November 17, 2022
  * Description: Logout page for Final Project
  */
-
+$_SESSION = [];
 session_destroy();
+//$_SESSION = array();
+//$_SESSION = array(); :((((((((((((((((((((
 header("location:index.php");
 ?>
