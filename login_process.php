e732a8a15ad529857fc824a8d2d1a56a1afc86ce
CPereira2School <cpereira2@academic.rrc.ca>
2022-12-02 13:57:48 -0600

    Shifted into the login.php page instead. This can be deleted(?)

--- login_process.php
+++ login_process.php
@@ -11,39 +11,5 @@
 
     //$_SESSION['user_id'] = "";
 
-    $username = filter_input(INPUT_POST, 'username', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-    $password = filter_input(INPUT_POST, 'password', FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-
-    if(isset($username) && isset($password))
-    {
-        $query = "SELECT * FROM user WHERE username ='$username'";
-
-        $statement = $db->prepare($query);
-        $statement->execute();
-        $row = $statement->fetch();
-
-        if($statement->rowCount() != 0)
-        {
-            $dbPassword = $row['password'];
-            if(password_verify($password, $dbPassword))
-            {
-                $_SESSION['user_id'] = $row['user_id'];
-                $_SESSION['username'] = $row['username'];
-                header("Location:index.php?success=yes");
-            }
-            else
-            {
-                header("Location:login.php?signup=password");
-            }
-        }
-        else
-        {
-            header("Location:login.php?signup=fail");
-        }
-
-    }
-    else
-    {
-        header("Location:login.php?signup=empty");
-    }
+   
 ?>
